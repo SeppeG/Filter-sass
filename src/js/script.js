b982a1668c71b4fa7b685cb70198aacb201dffc7
Seppe Geuens <geuensseppe@gmail.com>
2021-04-23 11:16:58 +0200

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,8 @@ $(function () {
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
 	target: "#navbar",
 });
+
+/* function isWorkingHour(now) {
+	let openDays = [2, 3, 4, 5, 6];
+	return openDays.includes(now.getDay()) && now.getHours() >= 9 && now.getHours() < 17.3;
+} */
